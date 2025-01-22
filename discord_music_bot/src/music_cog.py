613e44b78fad270fcdd5036b9591a33f87cdc47b
mattia.isetta <mattia.isetta@studenti.unimi.it>
2025-01-22 11:20:45 +0100

    add search func

--- discord_music_bot/src/music_cog.py
+++ discord_music_bot/src/music_cog.py
@@ -12,3 +12,14 @@ class music_cog(commands.Cog):
 
         self.music_queue = []
         self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+        self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+
+        self.vc = None
+
+    def search_yt(self, item):
+        with YoutubeDL(self.YDL_OPTIONS) as ydl:
+            try:
+                info = ydl.extract_info("ytsearch:%s" % item, download=False)['entries'][0]
+            except Exception:
+                return False
+        return {'source': info['formats'[0]['url']], 'title': info['title']}
